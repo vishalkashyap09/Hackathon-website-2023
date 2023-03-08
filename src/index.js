35257ec5d7965758c5a65b8829b2af7d46260681
Rishi Kumar <rsi.dev17@gmail.com>
2023-03-09 00:11:23 +0530

    fix mobile responsiveness

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@
       dd = String(today.getDate()).padStart(2, "0"),
       mm = String(today.getMonth() + 1).padStart(2, "0"),
       yyyy = today.getFullYear(),
-      nextYear = yyyy + 1,
-      dayMonth = "09/30/",
+      nextYear = yyyy,
+      dayMonth = "05/06/",
       birthday = dayMonth + yyyy;
   
   today = mm + "/" + dd + "/" + yyyy;
