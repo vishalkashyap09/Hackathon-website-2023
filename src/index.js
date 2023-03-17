0c62347d0396087a78e9cd0ae1ba8d05f971aaa8
OindrilaDas27 <oindrila.das2708@gmail.com>
2023-03-17 21:36:10 +0530

    add text type animation

--- src/index.js
+++ src/index.js
@@ -47,17 +47,73 @@
     }, 0);
 })();
 
+new TypeIt("#element", { 
+  lifeLike: false, 
+  speed: 0 
+})
+.type("s")
+.pause(168)
+.type("t")
+.pause(181)
+.type("a")
+.pause(138)
+.type("t")
+.pause(189)
+.type("y")
+.pause(84)
+.type("s")
+.pause(1282)
+.delete(1)
+.pause(203)
+.delete(1)
+.pause(1151)
+.type("u")
+.pause(69)
+.type("s")
+.pause(786)
+.type("_")
+.pause(469)
+.type("c")
+.pause(133)
+.type("o")
+.pause(10)
+.type("o")
+.pause(287)
+.type("d")
+.pause(186)
+.type("e")
+.pause(295)
+.move(-2)
+.pause(140)
+.delete(1)
+.pause(130)
+.move(null, { to: "END" })
+.pause(192)
+.delete(1)
+.pause(729)
+.type("e")
+.pause(501)
+.type("_")
+.pause(956)
+.type("0")
+.go();
+
 var collapsible = document.getElementsByClassName("faq-collapsible");
 var i;
 
 for (i = 0; i < collapsible.length; i++) {
   collapsible[i].addEventListener("click", function () {
     this.classList.toggle("active");
+    console.log('hello');
     var content = this.nextElementSibling;
     if (content.style.maxHeight) {
       content.style.maxHeight = null;
+      console.log('is it woking?');
     } else {
       content.style.maxHeight = content.scrollHeight + "em";
+      console.log('hmm');
     }
   });
+
+  
 }
