f3edbfd99e18794ea23f4c951e9a694afdc9cc6a
Rishi Kumar <rsi.dev17@gmail.com>
2023-03-18 11:43:40 +0530

    fix: apend 0 if the timer is single digit

--- index.js
+++ index.js
@@ -26,10 +26,11 @@
         const now = new Date().getTime(),
               distance = countDown - now;
 
-        document.getElementById("days").innerText = Math.floor(distance / (day)),
-          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+        // modified lines to print 2 digits with 0 appended at the start
+        document.getElementById("days").innerText = String(Math.floor(distance / (day))).padStart(2, "0");
+        document.getElementById("hours").innerText = String(Math.floor((distance % (day)) / (hour))).padStart(2, "0");
+        document.getElementById("minutes").innerText = String(Math.floor((distance % (hour)) / (minute))).padStart(2, "0");
+        document.getElementById("seconds").innerText = String(Math.floor((distance % (minute)) / second)).padStart(2, "0");
 
         //do something later when date is reached
         if (distance < 0) {
