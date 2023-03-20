952403a289db43384d63cb80cb908ff4f168458e
Rishi Kumar <rsi.dev17@gmail.com>
2023-03-20 18:14:18 +0530

    Add media queries for larger global scales

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 (function () {
   const second = 1000,
-        minute = second * 60,
-        hour = minute * 60,
-        day = hour * 24;
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
 
   //I'm adding this section so I don't have to keep updating this pen every year :-)
   //remove this if you don't need it
   let today = new Date(),
-      dd = String(today.getDate()).padStart(2, "0"),
-      mm = String(today.getMonth() + 1).padStart(2, "0"),
-      yyyy = today.getFullYear(),
-      nextYear = yyyy,
-      dayMonth = "05/06/",
-      birthday = dayMonth + yyyy;
-  
+    dd = String(today.getDate()).padStart(2, "0"),
+    mm = String(today.getMonth() + 1).padStart(2, "0"),
+    yyyy = today.getFullYear(),
+    nextYear = yyyy,
+    dayMonth = "05/06/",
+    birthday = dayMonth + yyyy;
+
   today = mm + "/" + dd + "/" + yyyy;
   if (today > birthday) {
     birthday = dayMonth + nextYear;
   }
   //end
-  
+
   const countDown = new Date(birthday).getTime(),
+  
       x = setInterval(function() {    
 
         const now = new Date().getTime(),
@@ -42,3 +43,55 @@
         //seconds
       }, 0)
   }());
+
+//status_code_0 animation
+new TypeIt("#element", { 
+  lifeLike: false, 
+  speed: 0 
+})
+.type("s")
+.pause(135)
+.type("t")
+.pause(141)
+.type("a")
+.pause(138)
+.type("t")
+.pause(102)
+.type("y")
+.pause(84)
+.type("s")
+.pause(300)
+.delete(1)
+.pause(187)
+.delete(1)
+.pause(357)
+.type("u")
+.pause(69)
+.type("s")
+.pause(243)
+.type("_")
+.pause(298)
+.type("c")
+.pause(133)
+.type("o")
+.pause(10)
+.type("o")
+.pause(287)
+.type("d")
+.pause(186)
+.type("e")
+.pause(295)
+.move(-2)
+.pause(140)
+.delete(1)
+.pause(130)
+.move(null, { to: "END" })
+.pause(192)
+.delete(1)
+.pause(329)
+.type("e")
+.pause(501)
+.type("_")
+.pause(956)
+.type("0")
+.go();
